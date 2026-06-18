65bf9a6f812caec602d1d45327ec4d1c43612507
Paulo Vitor Francisco <paulovfrancisco@gmail.com>
2026-06-18 14:14:53 -0300

    feat: create initial application (#2)
    * update .gitignore file
    
    * create initial Java Spring Boot application
    
    * create initial Java Spring Boot application
    
    * fix bug in database env var and change ci-backend.yml file
    
    * remove comment
    
    * Update src/main/resources/application-local.yml
    
    * Update src/main/resources/application-local.yml
    
    * Update src/main/resources/application.yml
    
    * Update src/main/resources/application.yml
    
    * Update pom.xml

---
+++ src/main/java/com/bestroute/BastRouteApplication.java
@@ -0,0 +1,13 @@
+package com.bestroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BastRouteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BastRouteApplication.class, args);
+    }
+
+}
