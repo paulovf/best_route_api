b641f78f19aec218c04460965acfbae55676885b
paulovf <paulovfrancisco@gmail.com>
2026-06-19 12:21:15 -0300

    create security classes

--- src/test/java/com/bestroute/BastRouteApplicationTests.java
+++ src/test/java/com/bestroute/BastRouteApplicationTests.java
@@ -1,17 +1,35 @@
 package com.bestroute;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureMockMvc
 class BastRouteApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void contextLoads() {
+    void deveBuscarRotasComSucessoQuandoChaveForValida() throws Exception {
+        mockMvc.perform(get("/api/routes")
+                .header("X-API-KEY", "default-api-key-test"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    void deveRetornar403QuandoNaoEnviarAChave() throws Exception {
+        mockMvc.perform(get("/api/routes"))
+            .andExpect(status().isForbidden());
     }
 
 }
