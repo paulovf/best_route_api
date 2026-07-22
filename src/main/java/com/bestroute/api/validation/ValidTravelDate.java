4f38e7c57cb0da88d269649909aa3ef26a3fc36e
Paulo Vitor Francisco <paulovfrancisco@gmail.com>
2026-07-22 15:11:03 -0300

    fix: add travel date validation and change README file (#18)
    * add travel date validation and change README file
    
    * change version

---
+++ src/main/java/com/bestroute/api/validation/ValidTravelDate.java
@@ -0,0 +1,19 @@
+package com.bestroute.api.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = TravelDateValidator.class)
+@Target({ ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidTravelDate {
+
+	String message() default "The travel date must be today or within 1 year from now";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+}
